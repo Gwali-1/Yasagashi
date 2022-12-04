5feb7a12f037983a6beafaf13ee375c4ee5cbb47
Gwali <gwalisam37@gmail.com>
2022-12-04 12:59:10 +0000

    added string representations to model objects in models.py

--- main/models.py
+++ main/models.py
@@ -18,6 +18,9 @@ class Profile(models.Model):
     email_verified=models.BooleanField(default=False)
     credibilty=models.IntegerField(default=0)
 
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
 
 
 class Listings(models.Model):
@@ -29,17 +32,25 @@ class Listings(models.Model):
     location=models.CharField(max_length=100)
     accomodation_type=models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"Listing by {self.user.username}"
+
 
 class Listing_favourites(models.Model):
     listing=models.ForeignKey(Listings,on_delete=models.CASCADE,related_name=("favourited_by"))
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name=("favourites"))
 
+    def __str__(self):
+        return f"Listing favourited by {self.user.username}"
 
 
 #when you give cred
 class Agents_favourite(models.Model):
     agent=models.ForeignKey(User,on_delete=models.CASCADE,related_name="faved_agents")
     user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f" Agent {self.user.username} cred  by {self.user.username}"
 
 
 
